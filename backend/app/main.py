3e4a12e73847f7a51e27ed1aee1415e3ba52443e
Andre Luis Ribeiro de Souza <andre09999@live.com>
2026-02-21 23:16:20 -0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -38,6 +38,7 @@ if not origins:
     origins = [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://business-insights-plataform.netlify.app/"
     ]
 
 app.add_middleware(
